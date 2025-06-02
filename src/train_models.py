b56bf8d1d2304fdbfd66148ed3cb64c90f028015
Mohamed Atef <31593703+MohamedElkaffas@users.noreply.github.com>
2025-06-02 07:07:56 +0300

    (fix): path

--- src/train_models.py
+++ src/train_models.py
@@ -55,8 +55,7 @@ def main():
     le = LabelEncoder()
     y_encoded = le.fit_transform(y_raw)
 
-    le_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),"..", "models", "label_encoder.pkl")
-    
+    le_path = os.path.join(os.path.dirname(__file__), "..", "models", "label_encoder.pkl")
     with open(le_path, "wb") as f_le:
         pickle.dump(le, f_le)
     print(f"Saved LabelEncoder to {le_path}")
@@ -91,7 +90,7 @@ def main():
     
     log_model_to_mlflow(lr, "Baseline_LogReg", X_train, y_train, lr_params, metrics_A)
     
-    svm_pickle_path = os.path.join(os.path.dirname(__file__),"..", "models", "best_hand_gesture.pkl")    
+    svm_pickle_path = os.path.join(os.path.dirname(__file__), "..", "models", "best_hand_gesture.pkl")    
     if os.path.exists(svm_pickle_path):
         print("Evaluating Loaded SVM...")
         with open(svm_pickle_path, "rb") as f_svm:
@@ -144,7 +143,7 @@ def main():
     best_model = models[best_idx]
     best_name = model_names[best_idx]
     
-    best_model_path = os.path.join(os.path.dirname(__file__), "models/best_hand_gesture.pkl")
+    best_model_path = os.path.join(os.path.dirname(__file__), "..", "models", "best_hand_gesture.pkl")
     with open(best_model_path, "wb") as f_best:
         pickle.dump(best_model, f_best)
     
